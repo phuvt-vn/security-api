8c2945e8cd221d2e7b348e3838dd0419fd69eeaa
Phu Vo <ldcc.thanhphu@lotte.vn>
2024-11-01 15:40:50 +0700

    comment configuration session cookie

--- src/main/java/com/example/main/api/filter/config/SessionCookieFilterConfig.java
+++ src/main/java/com/example/main/api/filter/config/SessionCookieFilterConfig.java
@@ -10,13 +10,13 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class SessionCookieFilterConfig {
 
-    @Autowired
+    //@Autowired
     private BasicAuthRepository basicAuthRepository;
 
-    @Autowired
+    //@Autowired
     private SessionCookieTokenService sessionCookieTokenService;
 
     @Bean
