7b4b4d6d9c0c272f938032a234f33d1169b2cc1f
Phu Vo <ldcc.thanhphu@lotte.vn>
2024-11-04 15:00:34 +0700

    Redis token + HMAC + simple UI

--- src/main/java/com/example/main/api/filter/config/RedisFilterConfig.java
+++ src/main/java/com/example/main/api/filter/config/RedisFilterConfig.java
@@ -12,13 +12,13 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class RedisFilterConfig {
 
-	@Autowired
+	//@Autowired
 	private BasicAuthRepository basicAuthRepository;
 
-	@Autowired
+	//@Autowired
 	private RedisTokenService redisTokenService;
 
 	@Bean
--- src/main/java/com/example/main/service/RedisTokenService.java
+++ src/main/java/com/example/main/service/RedisTokenService.java
@@ -1,6 +1,7 @@
 package com.example.main.service;
 
 import com.example.main.entity.RedisToken;
+import com.example.main.util.HmacUtil;
 import com.example.main.util.SecureStringUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,28 +21,41 @@ public class RedisTokenService {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private static final String HMAC_SECRET = "HMAC_SECRET";
+
     public String store (RedisToken token) {
         try {
             var tokenId = SecureStringUtil.randomString(30);
             var tokenJson = objectMapper.writeValueAsString(token);
             connection.sync().set(tokenId, tokenJson);
             connection.sync().expire(tokenId, 15*60);
-            return tokenId;
-        } catch (JsonProcessingException e) {
+
+            var hmac= HmacUtil.hmac(tokenId, HMAC_SECRET);
+
+            return StringUtils.join(tokenId,".",hmac);
+        } catch (Exception e) {
             e.printStackTrace();
             return StringUtils.EMPTY;
         }
     }
 
-    public Optional<RedisToken> read (String tokenId) {
-        var tokenJson = connection.sync().get(tokenId);
-        if (StringUtils.isBlank(tokenJson)) {
-            return Optional.empty();
-        }
+    public Optional<RedisToken> read (String bearerToken) {
+
         try {
+            var tokens = StringUtils.split(bearerToken, ".");
+
+            if(!HmacUtil.isHmacMatch(tokens[0],HMAC_SECRET,tokens[1])){
+                return Optional.empty();
+            }
+
+            var tokenJson = connection.sync().get(tokens[0]);
+            if (StringUtils.isBlank(tokenJson)) {
+                return Optional.empty();
+            }
+
             var token = objectMapper.readValue(tokenJson, RedisToken.class);
             return Optional.of(token);
-        } catch (JsonProcessingException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return Optional.empty();
         }
